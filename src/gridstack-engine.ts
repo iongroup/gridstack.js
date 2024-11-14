4dd3abbffb43b78b82dcb346e6349c6351d7c22a
Luciano Martorella <luciano.martorella@iongroup.com>
2024-11-14 14:54:19 +0100

    - Test fixes (PR: 2850)

--- src/gridstack-engine.ts
+++ src/gridstack-engine.ts
@@ -98,8 +98,12 @@ export class GridStackEngine {
     }
 
     let didMove = false;
-    let newOpt: GridStackMoveOpts = {nested: true, pack: false};
+    const newOpt: GridStackMoveOpts = {nested: true, pack: false};
+    let counter = 0;
     while (collide = collide || this.collide(node, area, opt.skip)) { // could collide with more than 1 item... so repeat for each
+      if (counter++ > this.nodes.length * 2) {
+        throw new Error("Infinite collide check");
+      }
       let moved: boolean;
       // if colliding with a locked item OR moving down with top gravity (and collide could move up) -> skip past the collide,
       // but remember that skip down so we only do this once (and push others otherwise).
